45ab4e4474005fcd8de2bd6e5649bb2abe047aff
Danielle Hillman <hillm108@mail.chapman.edu>
2020-08-03 13:59:53 -0700

    responsive

--- script.js
+++ script.js
@@ -78,7 +78,8 @@ function rain() {
   
       status.textContent = '';
       mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
-      mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+      // mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+     console.log(`Latitude: ${latitude} °, Longitude: ${longitude} °`);
     }
   
     function error() {
