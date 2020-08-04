8f3449ab9b3cc4f71986018055b5f51310b20849
Danielle Hillman <hillm108@mail.chapman.edu>
2020-08-04 10:10:27 -0700

    pushing

--- script.js
+++ script.js
@@ -7,63 +7,29 @@ var instance = M.Carousel.init({
 
 
 
-// let moodString = ''
-// let cityString = ''
-// let finalString = ''
-
-
-function cold (){
-
-
-}
-
-function midrange() {
-
-}
-
-function sunny () {
-
-
-}
-
-function rain() {
-
-
-}
-// {<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+{<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
  
-//     document.getElementById('submit').addEventListener('click', event => {
-//       event.preventDefault()
+    document.getElementById('submit').addEventListener('click', event => {
+      event.preventDefault()
 
-//       let city = document.getElementById('city').value
+      let city = document.getElementById('city').value
 
-//       axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=1dd25ac798a84daed3b612ef4b3c9a3e`)
-//         .then(res => {
-//           console.log(res.data)
-//           document.getElementById('test').innerHTML = `
-//           <h1>${res.data.name}</h1>
-//           <h2>Weather: ${res.data.weather[0].description}</h2>
-//           <h3>Temperature: ${res.data.main.temp}</h3>
+      axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=1dd25ac798a84daed3b612ef4b3c9a3e`)
+        .then(res => {
+          console.log(res.data)
+          document.getElementById('test').innerHTML = `
+          <h1>${res.data.name}</h1>
+          <h2>Weather: ${res.data.weather[0].description}</h2>
+          <h3>Temperature: ${res.data.main.temp}</h3>
         
-//         `
-//         })
+        `
+        })
 
-//         .catch(err => { console.log(err) })
+        .catch(err => { console.log(err) })
 
-//     })
+    })
 
 
-  //  if (res.data.weather[0].description === "rain") {
-  //    rain()
-   
-  //  } else if (res.data.main.temp < 60) {
-  //   cold()
-  // } else if (60 <= res.data.main.temp && res.data.main.temp < 75) {
-  //   midrange()
-
-  // } else if (res.data.main.temp >= 75) {
-  //   sunny()
-  // }
   function geoFindMe() {
 
     const status = document.querySelector('#status');
@@ -98,7 +64,7 @@ function rain() {
   document.querySelector('#find-me').addEventListener('click', geoFindMe);
 
 
-
+}
 
 
 
@@ -110,4 +76,4 @@ function rain() {
   // document.addEventListener('DOMContentLoaded', function() {
   //   var elems = document.querySelectorAll('.dropdown-trigger');
   //   var instances = M.Dropdown.init(elems, options);
-  // });
+  
