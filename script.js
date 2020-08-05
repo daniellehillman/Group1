3688f9bc67efdf591fe33871f61bee16e456bbf5
Matt Javier <matthewrjavier@gmail.com>
2020-08-04 19:07:46 -0700

    cleanup

--- script.js
+++ script.js
@@ -113,30 +113,18 @@ let moods = ['happy', 'stressed', 'chill', 'depressed', 'hyped']
 
 // global variables
 let mood
-let userLat
-let userLon
-let token = ''
 
 function geoFindMe() {
 
   const status = document.querySelector('#status')
-  // <<<<<<< HEAD
   const mapLink = document.querySelector('#map-link')
 
   mapLink.href = ''
   mapLink.textContent = ''
 
-  // =======
-  //   const mapLink = document.querySelector('#map-link')
-  //   mapLink.href = ''
-  //   mapLink.textContent = ''
-
-  // >>>>>>> b43c73781cf1365739ac12dfe27106141970eeb6
   function success(position) {
     const latitude = position.coords.latitude
     const longitude = position.coords.longitude
-    userLat = latitude
-    userLon = longitude
 
     status.textContent = ''
     mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`
@@ -165,7 +153,6 @@ function geoFindMe() {
     status.textContent = 'Locating…'
     navigator.geolocation.getCurrentPosition(success, error)
   }
-
 }
 
 function moodSelected() {
@@ -176,7 +163,6 @@ function moodSelected() {
     }
   })
   return count >= 1
-
 }
 
 function getWeatherData(username, usercity, usermood) {
@@ -205,8 +191,6 @@ function getWeatherData(username, usercity, usermood) {
       genre = 'jazz'
       break
   }
-
-
 
   // call to OpenWeatherAPI using city
   axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${usercity}&units=imperial&appid=b46e0399b18e0132c4c34e7071caa187`)
@@ -242,86 +226,81 @@ function getWeatherData(username, usercity, usermood) {
       // call to SpotifyAPI using danceability, energy, and genre parameters
       axios.get(`https://api.spotify.com/v1/recommendations?limit=10&market=US&seed_genres=${genre}&target_danceability=${danceability}&target_energy=${energy}`, {
         headers: {
-          'Authorization': `Bearer BQB2vki_U_9F1CneBrA-8e0KbgAxPXzYEmPWCQ9Q5K1dNIlJ7p52x85pNhPHFjTl7eGwB04ff7Zgtrre4BlAQ1iWQhkBDfCRnrQVWmPqg9Dc7Ns9p-1DdcXaSco1dWquG3w9l6R4Vvzl9UVmQ_iq7mHN`
-          // <<<<<<< HEAD
+          'Authorization': `Bearer BQBCfrNJvsGjydm2lPmCihGQxp8KcKIDuvgtG8TKIzjGJPQXnwNiYO-uRddP7CbAAfHKrh4Ej4yGNq1Ud4_ydIYZfvLb0xgMsDM80RWqfm3m2Vgm3rQtOzmUPesnXq8yhMdh4NYkERpIG0tTIhbE_UK0`
         }
       })
-        // =======
-        //         }})
-        // >>>>>>> b43c73781cf1365739ac12dfe27106141970eeb6
         .then(res => {
           console.log(res)
           let tracks = res.data.tracks
 
-          // display all tracks with artists, album names, and song titles
-          let playlist = document.getElementById('playlist')
-          playlist.innerHTML = `
-                  <button>View Playlist</button>
-                  <div id="1"></div>
-                  <div id="2"></div>
-                  <div id="3"></div>
-                  <div id="4"></div>
-                  <div id="5"></div>
-                  <div id="6"></div>
-                  <div id="7"></div>
-                  <div id="8"></div>
-                  <div id="9"></div>
-                  <div id="10"></div>
-                `
-          for (let i = 0; i < tracks.length; i++) {
-            console.log(`Artist: ${tracks[i].artists[0].name}, Album: ${tracks[i].album.name}, Song Title: ${tracks[i].name}`)
-
-            let playlist = document.getElementById('playlist')
-          }
+          // for (let i = 0; i < tracks.length; i++) {
+          //   console.log(`Artist: ${tracks[i].artists[0].name}, Album: ${tracks[i].album.name}, Song Title: ${tracks[i].name}`)
+          // }
             
+          let playlist = document.getElementById('playlist')
           let allImages = []
-          for (let i= 0; i < tracks.length; i++) {
+          for (let i = 0; i < tracks.length; i++) {
             image = tracks[i].album.images[0].url || "https://player.tritondigital.com/tpl/default/html5/img/player/default_cover_art.jpg"
             allImages.push(image)
           }
 
-          for (let i = 0; i < tracks.length; i++) {
+          let carousel = document.createElement('div')
+          carousel.className = 'carousel'
+
+          playlist.innerHTML = ''
+
+          for (let i = 0; i < allImages.length; i++) {
+            let carouselElem = document.createElement('a')
+            carouselElem.className = 'carousel-item'
+            carouselElem.href = `#${i}!`
+
+            let imgElem = document.createElement('img')
+            imgElem.src = `${allImages[i]}`
+
+            let divElem = document.createElement('div')
+            divElem.id = `${i}`
+
             axios.get(`https://api.lyrics.ovh/v1/${tracks[i].artists[0].name}/${tracks[i].name}`)
-              .then(res => {
-                console.log(res.data.lyrics)
-                let lyrics = res.data.lyrics
-                lyrics = lyrics.replace('\n', '<br>')
-                let playlist = document.getElementById('playlist')
-                // playlist.innerHTML = ''
-
-                document.getElementById(`${i}`).innerHTML = `
-                  <a class="carousel-item" href="#two!"><img src="${tracks[i].album.images[0].url}"> <div id="0">0</div></a>
-                  <p>Artist: ${tracks[i].artists[0].name} Song Title: ${tracks[i].name}</p>
-                  <p>${lyrics}</p>
-                `
-                $('.carousel').carousel();
-              })
-
-              .catch(err => {
-                // console.error(err)
-                document.getElementById(`${i}`).innerHTML = `
-                <a class="carousel-item" href="#two!"><img src="${tracks[i].album.images[0].url}"> <div id="0">0</div></a>
-                <p>Artist: ${tracks[i].artists[0].name} Song Title: ${tracks[i].name}. 
-                There is no lyrics for the song</p>
-                `
-                $('.carousel').carousel();
-              })
+              .then(res =>)
+            carouselElem.append(imgElem)
+            carouselElem.append(divElem)
+            carousel.append(carouselElem)
           }
-          playlist.innerHTML = `
-            <div class="carousel">
-              <a class="carousel-item" href="#two!"><img src="${allImages[0]}"> <div id="0">0</div></a>
-              <a class="carousel-item" href="#three!"><img src="${allImages[1]}"><div id="1">1</div></a>
-              <a class="carousel-item" href="#four!"><img src="${allImages[2]}"><div id="2">2</div></a>
-              <a class="carousel-item" href="#five!"><img src="${allImages[3]}"><div id="3">3</div></a>
-              <a class="carousel-item" href="#six!"><img src="${allImages[4]}"><div id="4">4</div></a>
-              <a class="carousel-item" href="#seven!"><img src="${allImages[5]}"><div id="5">5</div></a>
-            <a class="carousel-item" href="#eight!"><img src="${allImages[6]}"><div id="6">6</div></a>
-            <a class="carousel-item" href="#nine!"><img src="${allImages[7]}"><div id="7">7</div></a>
-            <a class="carousel-item" href="#ten!"><img src="${allImages[8]}"><div id="8">8</div></a>
-            <a class="carousel-item" href="#ten!"><img src="${allImages[9]}"><div id="9">9</div></a>
-          </div>
-          `
+
+          playlist.append(carousel)
+
+
+          // for (let i = 0; i < tracks.length; i++) {
+          //   axios.get(`https://api.lyrics.ovh/v1/${tracks[i].artists[0].name}/${tracks[i].name}`)
+          //     .then(res => {
+          //       console.log(res.data.lyrics)
+          //       let lyrics = res.data.lyrics
+          //       lyrics = lyrics.replace('\n', '<br>')
+          //       let playlist = document.getElementById('playlist')
+          //       // playlist.innerHTML = ''
+
+          //       document.getElementById(`${i}`).innerHTML = `
+          //         <a class="carousel-item" href="#two!"><img src="${tracks[i].album.images[0].url}"> <div id="0">0</div></a>
+          //         <p>Artist: ${tracks[i].artists[0].name} Song Title: ${tracks[i].name}</p>
+          //         <p>${lyrics}</p>
+          //       `
+          //       $('.carousel').carousel();
+          //     })
+
+          //     .catch(err => {
+          //       // console.error(err)
+          //       document.getElementById(`${i}`).innerHTML = `
+          //       <a class="carousel-item" href="#two!"><img src="${tracks[i].album.images[0].url}"> <div id="0">0</div></a>
+          //       <p>Artist: ${tracks[i].artists[0].name} Song Title: ${tracks[i].name}. 
+          //       There is no lyrics for the song</p>
+          //       `
+          //       $('.carousel').carousel();
+          //     })
+          //}
+
           $('.carousel').carousel();
+
+          
       
         })
         .catch(err => {
